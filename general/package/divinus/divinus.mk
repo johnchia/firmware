05cd71107eb9a429bfad068906f026533f055909
johnchia <johnchia@users.noreply.github.com>
2026-07-21 21:11:54 -0700

    ssc30kq: add Divinus-only firmware target

--- general/package/divinus/divinus.mk
+++ general/package/divinus/divinus.mk
@@ -9,11 +9,12 @@ DIVINUS_VERSION = HEAD
 DIVINUS_LICENSE = MIT
 DIVINUS_LICENSE_FILES = LICENSE
 
-# Keep camera-specific streaming changes confined to the ssc30kq ultimate
-# test image. All other targets continue to build the OpenIPC source.
-ifeq ($(OPENIPC_SOC_MODEL)-$(OPENIPC_VARIANT),ssc30kq-ultimate)
+# Keep the tested camera-specific streaming changes confined to the ssc30kq
+# Divinus images. All other targets continue to build the OpenIPC source.
+ifneq ($(filter ssc30kq-ultimate ssc30kq-divinus,$(OPENIPC_SOC_MODEL)-$(OPENIPC_VARIANT)),)
 DIVINUS_SITE = $(call github,johnchia,divinus,$(DIVINUS_VERSION))
 DIVINUS_VERSION = 849f4e5bf7cecb3f9d54e4be9581347f05268129
+DIVINUS_DEPENDENCIES += sigmastar-osdrv-infinity6e
 else
 define DIVINUS_APPLY_CONFIG_COMPAT_PATCH
 	$(APPLY_PATCHES) $(@D) $(DIVINUS_PKGDIR)/files/patches \
@@ -43,8 +44,6 @@ endef
 # Majestic normally owns the small Infinity6E library subset, so install the
 # complete vendor bundle here without changing any other SigmaStar image.
 ifeq ($(OPENIPC_SOC_MODEL)-$(OPENIPC_VARIANT),ssc30kq-ultimate)
-DIVINUS_DEPENDENCIES += sigmastar-osdrv-infinity6e
-
 define DIVINUS_INSTALL_SSC30KQ_ULTIMATE_RUNTIME
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/lib $(TARGET_DIR)/etc/init.d
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/usr/lib \
@@ -53,6 +52,18 @@ define DIVINUS_INSTALL_SSC30KQ_ULTIMATE_RUNTIME
 		$(TARGET_DIR)/etc/init.d/divinus
 endef
 DIVINUS_POST_INSTALL_TARGET_HOOKS += DIVINUS_INSTALL_SSC30KQ_ULTIMATE_RUNTIME
+endif
+
+# The Majestic-free target uses Divinus as its normal boot-time camera daemon.
+# The OSDRV package installs its full library bundle automatically when
+# Majestic is not selected.
+ifeq ($(OPENIPC_SOC_MODEL)-$(OPENIPC_VARIANT),ssc30kq-divinus)
+define DIVINUS_INSTALL_SSC30KQ_SERVICE
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
+	$(INSTALL) -m 755 $(DIVINUS_PKGDIR)/files/divinus \
+		$(TARGET_DIR)/etc/init.d/S95divinus
+endef
+DIVINUS_POST_INSTALL_TARGET_HOOKS += DIVINUS_INSTALL_SSC30KQ_SERVICE
 endif
 
 $(eval $(generic-package))
