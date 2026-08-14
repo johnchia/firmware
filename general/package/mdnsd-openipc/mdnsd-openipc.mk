1af356cd800bb5b86fc44d3c120980278f5ff940
johnchia <johnchia@users.noreply.github.com>
2026-08-14 08:09:30 -0700

    mdnsd: only install mquery when it was built
    MDNSD_OPENIPC_INSTALL_TARGET_CMDS installs src/mquery unconditionally, but
    the binary only exists when BR2_PACKAGE_MDNSD_MQUERY_OPENIPC selected
    --with-mquery. With the sub-option off -- which is what any configuration
    wanting just the daemon looks like -- the build gets as far as installing
    and then dies on
    
      install: cannot stat '.../mdnsd-openipc-v0.12/src/mquery': No such file
    
    No defconfig in the tree enables this package, which is presumably why it
    went unnoticed.

--- general/package/mdnsd-openipc/mdnsd-openipc.mk
+++ general/package/mdnsd-openipc/mdnsd-openipc.mk
@@ -20,13 +20,22 @@ else
 MDNSD_OPENIPC_CONF_OPTS += --without-mquery --without-systemd
 endif
 
+# mquery is only built when --with-mquery was passed, so installing it
+# unconditionally makes the package fail to install whenever the sub-option is
+# off -- which is every configuration that merely wants the daemon.
+ifeq ($(BR2_PACKAGE_MDNSD_MQUERY_OPENIPC),y)
+define MDNSD_OPENIPC_INSTALL_MQUERY
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(@D)/src/mquery
+endef
+endif
+
 define MDNSD_OPENIPC_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(MDNSD_OPENIPC_PKGDIR)/files/S50mdnsd
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/mdns.d
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/mdns.d $(MDNSD_OPENIPC_PKGDIR)/files/rtsp.service
-	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(@D)/src/mquery
+	$(MDNSD_OPENIPC_INSTALL_MQUERY)
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/lib
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/usr/lib $(@D)/libmdnsd/.libs/libmdnsd.so.1.0.0
 	ln -sf libmdnsd.so.1.0.0 $(TARGET_DIR)/usr/lib/libmdnsd.so
